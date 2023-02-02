0460c468849e7da4825afb2f18530981b988591f
Madoka <madokamiws@163.com>
2023-02-02 19:08:00 +0800

    登录回调测试

--- Assets/Scripts/ScreenRecording.cs
+++ Assets/Scripts/ScreenRecording.cs
@@ -14,14 +14,41 @@ public class ScreenRecording : MonoBehaviour
     private AsyncOperation ao;
     public bool ifLoadGameScene;
 
+    
     void Start()
     {
+       
+       StarkSDK.API.GetAccountManager().Login(OnLoginSuccessCallback,
+                   OnLoginFailedCallback,true);
+
+        
         startBtn.onClick.AddListener(StartVideo);
         //endBtn.onClick.AddListener(StopVideo);
         shareBtn.onClick.AddListener(ShareVideo);
         ao = SceneManager.LoadSceneAsync(1);
         ao.allowSceneActivation = false;
     }
+
+    /// <summary>
+    /// 登录成功回调
+    /// </summary>
+    /// <param name="code">临时登录凭证, 有效期 3 分钟。可以通过在服务器端调用 登录凭证校验接口 换取 openid 和 session_key 等信息。</param>
+    /// <param name="anonymousCode">用于标识当前设备, 无论登录与否都会返回, 有效期 3 分钟</param>
+    /// <param name="isLogin">判断在当前 APP(头条、抖音等)是否处于登录状态</param>
+    void OnLoginSuccessCallback(string code, string anonymousCode, bool isLogin)
+    {
+        Debug.Log("OnLoginSuccessCallback ... code：" + code + " ，anonymousCode：" + anonymousCode + " ，isLogin：" + isLogin);
+    }
+    /// <summary>
+    /// 检查Session接口调用失败的回调函数
+    /// </summary>
+    /// <param name="errMsg">错误原因</param>
+    void OnLoginFailedCallback(string errMsg)
+    {
+        Debug.Log("OnLoginFailedCallback ... errMsg：" + errMsg);
+    }
+
+
 
     void StartVideo()
     {
