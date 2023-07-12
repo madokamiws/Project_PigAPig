ffc874fb3a9a59b1d73b95dc4614e74717532790
Madoka <madokamiws@163.com>
2023-07-12 17:58:53 +0800

    修复倒计时显示bug，增加计时过了金币置暗

--- Assets/Scripts/GameScence/CountdownController.cs
+++ Assets/Scripts/GameScence/CountdownController.cs
@@ -11,32 +11,49 @@ namespace Yes.Game.Chicken
         public RectTransform goldRank2;
         public RectTransform goldRank3;
 
-        public Text tx_gold1;//1阶段奖励
+        public Text tx_gold1;//1等段奖励
 
-        public Text tx_gold2;//2阶段奖励
+        public Text tx_gold2;//2等段奖励
 
-        public Text tx_gold3;//3阶段奖励
+        public Text tx_gold3;//3等段奖励
+
+        public GameObject overgold1;
+
+        public GameObject overgold2;
+
+        public GameObject overgold3;
 
         private float countdownTime; // Set the countdown time
         private float currentTime;
         private float totalTime;
         private bool isPaused = true;
 
+        private float time_cover2;
+        private float time_cover3;
+
         public void SetupTimer(DeckModel deckmodel = null)
         {
+            overgold3.SetActive(false);
+            overgold2.SetActive(false);
+            overgold1.SetActive(false);
+
             if (deckmodel != null)
             {
-                float time_3 = deckmodel.time_3;
-                this.countdownTime = time_3;
+                float total_time = deckmodel.time_3;
+                this.countdownTime = total_time;
                 ResetTimer();
 
-                float _time2 = deckmodel.time_3 - deckmodel.time_2;
-                float _time3 = deckmodel.time_3 - deckmodel.time_1;
+                float _time1 = total_time - deckmodel.time_1;
+                float _time2 = total_time - deckmodel.time_2;
+                float _time3 = 0;
+
+                time_cover2 = _time1;
+                time_cover3 = _time2;
 
                 // Calculate the ratios and set the positions of the gold ranks
-                float ratio1 = 0;
-                float ratio2 = _time2 / deckmodel.time_3;
-                float ratio3 = _time3 / deckmodel.time_3;
+                float ratio1 = _time1 / total_time;
+                float ratio2 = _time2 / total_time;
+                float ratio3 = 0;
 
                 tx_gold3.text = deckmodel.gold_3.ToString();
                 tx_gold2.text = deckmodel.gold_2.ToString();
@@ -52,9 +69,12 @@ namespace Yes.Game.Chicken
                 countdownTime = 20f;
                 ResetTimer();
 
-                float ratio1 = 0;
-                float ratio2 = (float)7 / 20;
-                float ratio3 = (float)15 / 20;
+                float ratio1 = (float)15 / 20;
+                float ratio2 = (float)5 / 20;
+                float ratio3 = 0;
+                time_cover2 = 15f;
+                time_cover3 = 5f;
+
 
                 tx_gold1.text = "30";
                 tx_gold2.text = "50";
@@ -80,10 +100,23 @@ namespace Yes.Game.Chicken
                 totalTime += Time.deltaTime;
                 if (currentTime > 0)
                 {
- 
+
                     currentTime -= Time.deltaTime;
                     //Debug.Log("currentTime = " + currentTime);
+                    if (currentTime < time_cover2)
+                    {
+                        overgold1.SetActive(true);
+                    }
+                    if (currentTime < time_cover3)
+                    {
+                        overgold2.SetActive(true);
+                    }
+
                     timerSlider.value = Math.Max(currentTime / countdownTime, 0); // Ensure the slider value won't go below 0
+                }
+                else
+                {
+                    overgold3.SetActive(true);
                 }
             }
         }
